c5182f2370030e252b6608ea895050490d3723ca
believer <rickard.laurin@gmail.com>
2017-03-26 20:46:47 +0200

    Remake IDs with user ID and movie ID to be unique

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ApolloClient, { createNetworkInterface } from 'apollo-client'
 import VueApollo from 'vue-apollo'
 
-// Create the apollo client
 const apolloClient = new ApolloClient({
   networkInterface: createNetworkInterface({
     uri: 'http://movies-api-graphql.33e99398.svc.dockerapp.io:4000/graphql',
-    transportBatching: true,
+    transportBatching: true
   }),
+  addTypename: true,
+  dataIdFromObject: r => {
+    if (r.id && r.user) {
+      return `${r.user.id}${r.id}`
+    }
+    return null
+  }
 })
 
 // Install the vue plugin
